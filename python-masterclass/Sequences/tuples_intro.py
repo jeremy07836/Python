5ff70411f6a2aefdddec0e4186261619ec81ea4d
jeremy07836 <jeremy07836@gmail.com>
2024-11-05 08:57:15 -0500

    Continuing to add to the Sequence project number 4

--- python-masterclass/Sequences/tuples_intro.py
+++ python-masterclass/Sequences/tuples_intro.py
@@ -27,11 +27,26 @@ table = ("Coffee table", 200, 100, 75, 34.50)
 name, length, width, height, price = table
 print(length * width)
 
-albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
-          ("Bad Company", "Bad Company", 1974),
-          ("Nightflight", "Budgie", 1981),
-          ("More Mayhem", "Emilda May", 2011),
-          ("Ride the Lightning", "Metallica", 1984),
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975,
+           (
+               (1, "Song1")
+           )),
+          ("Bad Company", "Bad Company", 1974,
+           (
+               (1, "Song1")
+           )),
+          ("Nightflight", "Budgie", 1981,
+           (
+               (1, "Song1")
+           )),
+          ("More Mayhem", "Emilda May", 2011,
+           (
+               (1, "Song1")
+           )),
+          ("Ride the Lightning", "Metallica", 1984,
+           (
+               (1, "Song1")
+           )),
           ]
 
 print(len(albums))
