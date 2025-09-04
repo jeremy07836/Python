db88b0a51b68fed821cfd730a24d73a9879af5a9
jeremy07836 <jeremy07836@gmail.com>
2025-09-04 08:39:36 -0400

    Project oop 04, more doc strings

--- python-masterclass/oop/song.py
+++ python-masterclass/oop/song.py
@@ -7,13 +7,20 @@ class Song:
     """
 
     def __init__(self, title, artist, duration=0):
-        """Song init method
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
+
+# help(Song.__init__)
+print(Song.__doc__)
+print(Song.__init__.__doc__)
+Song.__init__.__doc__ = """Song init method
         Args:
             title (str): Initialises the `title` attribute
             artist (Artist): An Artist object representing the songs creator
             duration (Optional[int]): Initial value of the `duration` attribute
                 will default to zero if not specified
         """
-        self.title = title
-        self.artist = artist
-        self.duration = duration
+
+help(Song)
