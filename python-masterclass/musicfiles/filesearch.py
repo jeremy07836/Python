fa0b2e32ba06d3acd27a14b6a57b28f7c80cc4d1
jeremy07836 <jeremy07836@gmail.com>
2025-10-24 10:27:09 -0400

    Project musicfiles 02, worked on find and reading mp3 and other files

--- python-masterclass/musicfiles/filesearch.py
+++ python-masterclass/musicfiles/filesearch.py
@@ -19,11 +19,27 @@ def find_songs(albums):
         for song in os.listdir(album[0]):   # we want the path, not the name of the album
             yield song
 
+
+def get_song_path(albums):
+    for path in albums:
+        album_dir = path[0]
+        for _, _, songs in os.walk(album_dir):
+            for song in songs:
+                # print(path, song)
+                fullpath = album_dir + '\\\\' + song
+                yield fullpath
+
 album_list = find_albums("music", "Black*")
+
 song_list = find_songs(album_list)
+
+songs = get_song_path(album_list)
+for song_path in songs:
+    print(song_path)
 
 # for a in album_list:
 #     print(a)
 
 for s in song_list:
-    print(s)
+    # print(s)
+    pass
---
+++ python-masterclass/musicfiles/mp3scan.py
@@ -0,0 +1,28 @@
+import os
+import fnmatch
+import id3reader_p3 as id3reader
+
+
+def find_music(start, extenstion):
+    for path, directories, files in os.walk(start):
+        for file in fnmatch.filter(files, '*.{}'.format(extenstion)):
+            absolute_path = os.path.abspath(path)
+            yield os.path.join(absolute_path, file)
+
+errors = []
+for f in find_music('music', 'emp3'):
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
+            id3r.get_value('performer'),
+            id3r.get_value('album'),
+            id3r.get_value('track'),
+            id3r.get_value('title')
+        ))
+    except OSError:
+        # print("This file can not be read:")
+        # print("\t{}".format(f))
+        errors.append(f)
+
+for error_file in errors:
+    print(error_file)
