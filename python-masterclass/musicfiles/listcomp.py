9b9313c07b193acf83569e654414bb746c4a2c49
jeremy07836 <jeremy07836@gmail.com>
2025-11-07 13:32:59 -0500

    Project musicfiles 03, worked on list comprehensions

---
+++ python-masterclass/musicfiles/listcomp.py
@@ -0,0 +1,9 @@
+print(__file__)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+# squares = [number ** 2 for number in numbers]
+squares = [number ** 2 for number in range(1, 7)]
+# squares = {number ** 2 for number in numbers}
+
+print(squares)
