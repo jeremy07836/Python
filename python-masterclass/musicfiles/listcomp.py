dd2dd3a7c210c5c9883569d88326f230755deefe
jeremy07836 <jeremy07836@gmail.com>
2025-12-22 09:17:01 -0500

    Project musicfiles 04, worked on list comprehensions challenges

--- python-masterclass/musicfiles/listcomp.py
+++ python-masterclass/musicfiles/listcomp.py
@@ -2,8 +2,11 @@ print(__file__)
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# squares = [number ** 2 for number in numbers]
-squares = [number ** 2 for number in range(1, 7)]
+number = int(input("Please enter a number, and I'll tell you its square: "))
+
+squares = [number ** 2 for number in numbers]
+# squares = [number ** 2 for number in range(1, 7)]
 # squares = {number ** 2 for number in numbers}
 
-print(squares)
+index_pos = numbers.index(number)
+print(squares[index_pos])
